fde2b5c6e1a9d386290f71bbf3276dec09a5fcdd
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2023-09-13 00:33:04 +0700

    oke

--- main.go
+++ main.go
@@ -3,29 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	// port := "5001"
+	// port := os.Getenv("PORT")
+	port := "5001"
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	// router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-	router.Static("/build/web", "build/web")
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// // router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+	// router.Static("/build/web", "build/web")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.html", nil)
+	// })
 
-	router.Run(":" + port)
+	fileServer := http.FileServer(http.Dir("./build/web")) // New code
+	http.Handle("/", fileServer)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// router.Run(":" + port)
 }
