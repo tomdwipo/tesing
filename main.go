97bc2a6d1b8952892b1932b3518dfb50dc331f06
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2023-09-13 00:15:38 +0700

    commit

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 	router.Static("/build/web", "build/web")
 
